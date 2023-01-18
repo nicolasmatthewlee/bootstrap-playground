0d94832d852418f1dccd7c9eb61b83f242fd152f
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-18 10:19:09 -0800

    worked through typography

--- src/App.js
+++ src/App.js
@@ -140,7 +140,6 @@ function App() {
           <div className="col-4 border">col-4</div>
           <div className="col-7 border offset-1">col-7</div>
         </div>
-
         {/* remove negative margins and padding from child columns
         by setting gutters to 0: g-0 */}
         <div className="row g-0">
@@ -148,7 +147,6 @@ function App() {
           <div className="col-4 border">col-4</div>
           <div className="col-4 border">col-4 </div>
         </div>
-
         {/* create equal-width columns that span multiple rows by inserting
         w-100 where you want the columns to break to a new line */}
         <div className="row border">
@@ -158,12 +156,157 @@ function App() {
           <div className="col border">col</div>
           <div className="col border">col</div>
         </div>
-
         {/* fixed layout : using pixel sizes for a one-structure, screen-size-specific layout
         fluid layout : using percent sizes for a one-structure fluid layout
         responsive layout : using breakpoints to define multiple structures that adapt responsively to different sizes
         (^^ for example defining different column sizes for sm, md, lg screen sizes)
         responsive layouts enable an optimal layout at each screen size */}
+        {/* headers in Bootstrap */}
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+        <h5>h5</h5>
+        <h6>h6</h6>
+        <br />
+        {/* display ssecondary text of a heading in a smaller and lighter variation
+        with small tag and text-muted className */}
+        <h2>
+          h2
+          <small className="text-muted"> text-muted</small>
+        </h2>
+        <br />
+        {/* Bootstrap headings feature a larger font-size with a lighter font-weight */}
+        <h1 className="display-1">display heading 1</h1>
+        <h1 className="display-2">display heading 2</h1>
+        <h1 className="display-3">display heading 3</h1>
+        <h1 className="display-4">display heading 4</h1>
+        <h1 className="display-5">display heading 5</h1>
+        <h1 className="display-6">display heading 6</h1>
+        <br />
+        {/* paragraphs in bootstrap - use className lead for a larger font-size and lighter font-weight */}
+        <p>paragraph in Bootstrap</p>
+        <p className="lead">paragraph lead in Bootstrap</p>
+        <br />
+
+        {/* align text to left, center, right with text-start, text-center, text-end
+        can also set align for different screen sizes (text-sm-center
+        text-md-right) */}
+        <p className="text-start">text-start</p>
+        <p className="text-center">text-center</p>
+        <p className="text-end">text-end</p>
+        <br />
+
+        {/* can format text using standard html tags
+
+        b-bold
+        code-code
+        em-italicize (emphasized)
+        i-italicize
+        mark-highlight
+        strong-bold (strongly emphasized)
+        small-smaller
+        sub-subscript
+        sup-superscript
+        ins-underlined (inserted)
+        del-strikethrough (deleted) */}
+        <p>
+          <b>b</b>
+        </p>
+        <p>
+          <code>code</code>
+        </p>
+        <p>
+          <em>em</em>
+        </p>
+        <p>
+          <i>i</i>
+        </p>
+        <p>
+          <mark>mark</mark>
+        </p>
+        <p>
+          <strong>strong</strong>
+        </p>
+        <p>
+          <small>small</small>
+        </p>
+        <p>
+          <sub>sub</sub>
+          <sup>sup</sup>
+        </p>
+        <p>
+          <ins>ins</ins>
+        </p>
+        <p>
+          <del>del</del>
+        </p>
+        <br />
+
+        {/* can format uppercase, lowercase, capitalize with text-uppercase, text-lowercase, text-capitalize */}
+        <p className="text-lowercase">
+          text-lowercase: The quick brown fox jumps over the lazy dog.
+        </p>
+        <p className="text-uppercase">
+          text-uppercase: The quick brown fox jumps over the lazy dog.
+        </p>
+        <p className="text-capitalize">
+          text-capitalize: The quick brown fox jumps over the lazy dog.
+        </p>
+        <br />
+
+        {/* utility classes can be used to set font-color */}
+        <p className="text-primary">text-primary</p>
+        <p className="text-secondary">text-secondary</p>
+        <p className="text-success">text-success</p>
+        <p className="text-info">text-info</p>
+        <p className="text-warning">text-warning</p>
+        <p className="text-danger">text-danger</p>
+        <p className="text-muted">text-muted</p>
+
+        {/* blockquotes in Bootstrap - looks like a larger version of p */}
+        <blockquote className="blockquote">
+          <p>paragraph in blockquote with class blockquote</p>
+        </blockquote>
+
+        {/* for a properly attributed blockquote, put the blockquote 
+        (with class blockquote) in a figure tag and use a figcaption 
+        with class blockquote-footer that includes the author within
+        cite tags; can align with text-end text-center */}
+        <figure>
+          <blockquote className="blockquote">
+            <p>paragraph in blockquote in figure</p>
+          </blockquote>
+          <figcaption className="blockquote-footer">
+            figcaption.blockquote-footer by <cite>cite html tag</cite>
+          </figcaption>
+        </figure>
+
+        {/* truncate text with an ellipsis with text-truncate */}
+        <div className="row">
+          <div className="col-2 text-truncate">
+            The quick brown fox jumps over the lazy dog.
+          </div>
+        </div>
+
+        {/* text-wrap wraps text
+        text-nowrap prevents text wrapping */}
+        <div className="row">
+          <div className="col-2 text-nowrap bg-primary">
+            <p>The quick brown fox jumps over the lazy dog.</p>
+          </div>
+        </div>
+        <br />
+
+        {/* use text-break to prevent a long word from breaking the layout */}
+        <div className="row">
+          <div className="col-1 border">
+            <p>The quickbrownfox jumps over the lazy dog.</p>
+          </div>
+          <div className="col-1 text-break border order-first">
+            <p>The quickbrownfox jumps over the lazy dog.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
